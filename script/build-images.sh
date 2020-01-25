0301bde20b5b04782604d94bd74fa866cfbd7632
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2020-01-25 21:23:40 +0700

    update image

--- script/build-images.sh
+++ script/build-images.sh
@@ -1,2 +1,2 @@
-docker build -f Dockerfile -t gcr.io/majestic-bounty-235008/smilegan:0.0.3 ./
-docker push gcr.io/majestic-bounty-235008/smilegan:0.0.3
+docker build -f Dockerfile -t gcr.io/majestic-bounty-235008/smilegan:0.0.4 ./
+docker push gcr.io/majestic-bounty-235008/smilegan:0.0.4
