02517667e7203ddcaec8c8c67c57ae975ade1224
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2020-07-24 05:57:28 +0700

    add testing notebook

--- trainer/evaluator.py
+++ trainer/evaluator.py
@@ -1,5 +1,5 @@
 from .fid_calculator import FIDCalculator
-from .input import get_test_input, process_test, denormalize
+from .input import get_test_input, preprocess_test, process_test, denormalize, AUTOTUNE
 from .model import SmileGan
 import argparse
 import tensorflow as tf
@@ -26,9 +26,24 @@ class Evaluator:
             self.fid_calculator.calc_stats(stats_data_gen)
 
     def get_smile(self, image_path):
+        """
+        Get smile form path
+        """
         img_test = tf.expand_dims(process_test(image_path), 0)
         return denormalize(self.model.generator_g(img_test)[0])
+    
+    def get_smiles(self, images_dataset):
+        """
+        Get smile form multiple image.
+        images_dataset must be tf.Dataset of image matrix
+        """
+        img_test = images_dataset.map(preprocess_test, num_parallel_calls=AUTOTUNE).batch(5)
+        return denormalize(self.model.generator_g.predict(img_test))
 
     def calculate_fid_single(self, image_generated):
         img_test = tf.expand_dims(image_generated, 0)
         return self.fid_calculator.calculate(img_test)
+    
+    def calculate_fid(self, image_generated):
+        return self.fid_calculator.calculate(image_generated)
+    
